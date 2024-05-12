cb0f19a654b86a5928f9e5efa1f1185ef4d9a730
Kyle Cubit <kjc70@students.uwf.edu>
2024-05-12 00:06:34 -0500

    Add tesseract path selection button

--- TextExtractor.py
+++ TextExtractor.py
@@ -1,13 +1,20 @@
-from PIL import Image 
-from pytesseract import pytesseract 
+from PIL import Image
+from pytesseract import pytesseract
+import tkinter as tk
+
 # pytesseract.tesseract_cmd = "/usr/bin/tesseract"
 # print(pytesseract.image_to_string(Image.open("../sampletext.png")))
+
 
 class TextExtractor:
     def __init__(self):
         self.tesseract_cmd_path = "/usr/bin/tesseract"
-   
-  
+
     def extractText(self, imgPath):
         text = pytesseract.image_to_string(Image.open(imgPath))
         return text
+
+    def selectPath(self):
+        folder_selected = tk.filedialog.askdirectory()
+        self.tesseract_cmd_path = folder_selected
+        return folder_selected
--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import tkinter as tk
 from tkinter import filedialog
 from typing import Text
-from PIL import Image 
-from pytesseract import pytesseract 
-from Extractor import *
+from PIL import Image
+from pytesseract import pytesseract
 from PIL import Image, ImageTk
 
 import TextExtractor
+
 extractor = TextExtractor.TextExtractor()
 
+
 def open_image():
-    file_path = filedialog.askopenfilename(title="Open Image File", filetypes=[("Image files", "*.png *.jpg *.jpeg *.gif *.bmp *.ico")])
-    if file_path:       
+    file_path = filedialog.askopenfilename(
+        title="Open Image File",
+        filetypes=[("Image files", "*.png *.jpg *.jpeg *.gif *.bmp *.ico")],
+    )
+    if file_path:
         text = extractor.extractText(file_path)
         display_image(file_path)
+
 
 def display_image(file_path):
     image = Image.open(file_path)
@@ -25,8 +30,10 @@ def display_image(file_path):
 
 root = tk.Tk()
 root.title("Extract Text from Image")
-
-
+path_button = tk.Button(
+    root, text="Select Path to Tesseract", command=extractor.selectPath
+)
+path_button.pack(padx=20, pady=10)
 
 # text_widget = tk.Text(root, wrap=tk.WORD, height=15, width=35)
 open_button = tk.Button(root, text="Select Image", command=open_image)
@@ -35,6 +42,6 @@ image_label = tk.Label(root)
 image_label.pack(padx=20, pady=20)
 status_label = tk.Label(root, text="", padx=20, pady=10)
 status_label.pack()
-# pytesseract.tesseract_cmd = "/usr/bin/tesseract"
+
 
 root.mainloop()
